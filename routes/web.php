02aef6878af4a081218d40b2c852fa607e0d2f38
張雅茹 <3a832060@gm.student.ncut.edu.tw>
2021-11-15 05:02:55 +0800

    為Route命名,增加超連結

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-//設定Route回傳字串
-Route::get('/',function(){
-    return 'welcome';
-});
+////設定Route回傳字串
+//Route::get('/',function(){
+//    return 'welcome';
+//});
 //設定Route轉跳路由
 Route::get('r1',function (){
     return redirect('r2');
@@ -27,14 +27,14 @@ Route::get('r1',function (){
 Route::get('r2',function (){
     return view('welcome');
 });
-//修改Route接受參數
-Route::get('hello/{name}',function ($name){
-    return 'Hello, '.$name;
-});
+////修改Route接受參數
+//Route::get('hello/{name}',function ($name){
+//    return 'Hello, '.$name;
+//});
 //修改參數選擇性
 Route::get('hello/{name?}',function ($name='Everybody'){
     return 'Hello, '.$name;
-});
+})->name('hello.index');
 //新增Route觀察artisan
 Route::get('world',function (){
     return 'Hello, world!!!';
